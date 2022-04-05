02d0ec09639022ae5e7f15e824f330c0f4f76ad9
Vadim <vadlen2149@gmail.com>
2022-04-05 10:17:18 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { nanoid } from 'nanoid'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form';
+
+  public newFormGroup: FormGroup  = new FormGroup ( {
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    price: new FormControl(null, [Validators.required, Validators.min(0)]),
+    id: new FormControl(nanoid()),
+    categories: new FormControl([])
+  })
+
+  public generateButtonClick(): void {
+     this.newFormGroup.get('id')?.setValue(nanoid());
+  }
+
+  public onSubmitButtonClick(): void {
+    if (this.newFormGroup.invalid === true) {
+      alert('This form is not valid!')
+    } else {
+      console.log(this.newFormGroup);
+      
+    }
+  }
 }
